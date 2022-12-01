83d4ae5cc563255344420f96891f19ee0ec0b4fc
Louis Smith <15144861+smudger@users.noreply.github.com>
2022-12-01 21:49:41 +0000

    Improve output from solver

--- src/Solver.php
+++ src/Solver.php
@@ -11,13 +11,33 @@ class Solver
         $puzzle = $args[1];
 
         $class = "Smudger\\AdventOfCode2022\\Day$day\\Puzzle$puzzle";
+        if (! class_exists($class)) {
+            self::error("Could not find class to solve Day $day Puzzle $puzzle.");
+            self::info("Tried to instantiate: $class");
+            exit(1);
+        }
+
         $instance = new $class();
+        if (! is_callable($instance)) {
+            self::error("Instantiated class is not callable: $class");
+            exit(1);
+        }
 
         $startTime = microtime(true);
-        echo is_callable($instance) ? $instance('input.txt') : 'Instantiated class is not callable.';
+        self::info($instance('input.txt'));
         $endTime = microtime(true);
 
-        $elapsed = round($endTime-$startTime, 4);
-        echo "\nCompleted in $elapsed seconds.\n";
+        $elapsed = round($endTime - $startTime, 4);
+        self::info("Completed in $elapsed seconds.");
+    }
+
+    private static function error(string $message): void
+    {
+        echo "\033[01;31m  $message  \033[0m\n";
+    }
+
+    private static function info(string $message): void
+    {
+        echo "  $message  \n";
     }
 }
