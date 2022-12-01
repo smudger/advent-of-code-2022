fc4d6470156fba8a60a759c9169c1d6d89ca672b
Louis Smith <15144861+smudger@users.noreply.github.com>
2022-12-01 19:57:51 +0000

    Add php quality of life features

---
+++ src/Day1/Puzzle1.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Smudger\AdventOfCode2022\Day1;
+
+class Puzzle1
+{
+}
---
+++ tests/Day1/Puzzle1Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Smudger\AdventOfCode2022\Tests\Day1;
+
+use PHPUnit\Framework\TestCase;
+use Smudger\AdventOfCode2022\Day1\Puzzle1;
+
+class Puzzle1Test extends TestCase
+{
+}
