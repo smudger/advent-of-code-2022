c3d1cdf19e57f78ebaa557ec9c17fd2c12d7308f
Louis Smith <15144861+smudger@users.noreply.github.com>
2022-12-06 18:15:46 +0000

    Solve day 6

---
+++ src/Day6/Puzzle2.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Smudger\AdventOfCode2022\Day6;
+
+use Exception;
+use Illuminate\Support\Collection;
+
+class Puzzle2
+{
+    public function __invoke(string $fileName): int
+    {
+        $input = file_get_contents(__DIR__.'/'.$fileName)
+            ?: throw new Exception('Failed to read input file.');
+        $buffer = str_split($input);
+        $startIndex = (new Collection($buffer))
+            ->map(fn (string $_, int $index) => array_splice($buffer, $index, 14))
+            ->where(fn (array $splice) => array_unique($splice) === $splice)
+            ->keys()
+            ->first();
+
+        return $startIndex + 14;
+    }
+}
---
+++ tests/Day6Test.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Smudger\AdventOfCode2022\Tests;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+use Smudger\AdventOfCode2022\Day6\Puzzle1;
+use Smudger\AdventOfCode2022\Day6\Puzzle2;
+
+class Day6Test extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider getPuzzle1Examples()
+     */
+    public function it_returns_the_correct_answer_for_puzzle_1($example, $answer): void
+    {
+        Assert::assertEquals($answer, (new Puzzle1())($example));
+    }
+
+    public function getPuzzle1Examples()
+    {
+        return [
+            ['example1.txt', 7],
+            ['example2.txt', 5],
+            ['example3.txt', 6],
+            ['example4.txt', 10],
+            ['example5.txt', 11],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider getPuzzle2Examples()
+     */
+    public function it_returns_the_correct_answer_for_puzzle_2_example_1($example, $answer): void
+    {
+        Assert::assertEquals($answer, (new Puzzle2())($example));
+    }
+
+    public function getPuzzle2Examples()
+    {
+        return [
+            ['example6.txt', 19],
+            ['example7.txt', 23],
+            ['example8.txt', 23],
+            ['example9.txt', 29],
+            ['example10.txt', 26],
+        ];
+    }
+}
