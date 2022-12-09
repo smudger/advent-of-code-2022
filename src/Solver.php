4ed358dc88e6ff33d702822f749f4254e0cce036
Louis Smith <15144861+smudger@users.noreply.github.com>
2022-12-09 16:37:53 +0000

    Add ability to run all solutions

--- src/Solver.php
+++ src/Solver.php
@@ -2,14 +2,44 @@
 
 namespace Smudger\AdventOfCode2022;
 
+use Illuminate\Support\Collection;
+
 class Solver
 {
     public static function solve(\Composer\Script\Event $event): void
     {
         $args = $event->getArguments();
-        $day = $args[0];
-        $puzzle = $args[1];
 
+        if ($args[0] === 'all') {
+            self::runAll();
+        } else {
+            $day = $args[0];
+            $puzzle = $args[1];
+
+            self::run($day, $puzzle);
+        }
+    }
+
+    private static function runAll(): void
+    {
+        $startTime = microtime(true);
+        (new Collection(scandir(__DIR__)))
+            ->filter(fn (string $dir) => str_starts_with($dir, 'Day'))
+            ->map(fn (string $dir) => substr($dir, 3))
+            ->each(function (string $day) {
+                self::run($day, '1');
+                self::info('');
+                self::run($day, '2');
+                self::info('');
+            });
+        $endTime = microtime(true);
+
+        $elapsed = round($endTime - $startTime, 4);
+        self::success("Completed all solutions in $elapsed seconds.");
+    }
+
+    private static function run($day, $puzzle): void
+    {
         $class = "Smudger\\AdventOfCode2022\\Day$day\\Puzzle$puzzle";
         if (! class_exists($class)) {
             self::error("Could not find class to solve Day $day Puzzle $puzzle.");
@@ -23,12 +53,21 @@ class Solver
             exit(1);
         }
 
+        $prettyDay = str_pad($day, 2, '0', STR_PAD_LEFT);
+        self::info("========= Day $prettyDay, Puzzle $puzzle =========");
+
         $startTime = microtime(true);
-        self::info($instance('input.txt'));
+        self::success($instance('input.txt'));
         $endTime = microtime(true);
 
         $elapsed = round($endTime - $startTime, 4);
         self::info("Completed in $elapsed seconds.");
+        self::info('====================================');
+    }
+
+    private static function success(string $message): void
+    {
+        echo "\033[01;32m  $message  \033[0m\n";
     }
 
     private static function error(string $message): void
