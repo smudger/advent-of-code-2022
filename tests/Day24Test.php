7322871e6c7a6c06d42d05208dac54b034c76ddf
Louis Smith <15144861+smudger@users.noreply.github.com>
2022-12-26 21:50:44 +0000

    Solve day 24 puzzle 1

---
+++ tests/Day24Test.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Smudger\AdventOfCode2022\Tests;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+use Smudger\AdventOfCode2022\Day24\Puzzle1;
+use Smudger\AdventOfCode2022\Day24\Puzzle2;
+
+class Day24Test extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider getPuzzle1Examples()
+     */
+    public function it_returns_the_correct_answer_for_puzzle_1($example, $answer): void
+    {
+        Assert::assertEquals($answer, (new Puzzle1())($example));
+    }
+
+    public function getPuzzle1Examples()
+    {
+        return [
+            ['example1.txt', 18],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider getPuzzle2Examples()
+     */
+    public function it_returns_the_correct_answer_for_puzzle_2($example, $answer): void
+    {
+        Assert::assertEquals($answer, (new Puzzle2())($example));
+    }
+
+    public function getPuzzle2Examples()
+    {
+        return [
+            ['example1.txt', 19],
+        ];
+    }
+}
